ceb6f61fee44330053e244e449ac6bb5e1b96198
root <root@localhost.localdomain>
2018-03-25 19:30:28 +0530

    first commit

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "2016ucs0024@iitjammu.ac.in";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
