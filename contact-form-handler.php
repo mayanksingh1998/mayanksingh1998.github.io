be0d1e888202b3c510725247c28f55fe81a83798
root <root@localhost.localdomain>
2018-03-25 16:20:50 +0530

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email=$_POST['email'];
+	$message = $_POST['message'];
+
+
+	$email_form = 'mayanksingh1998.github.io';
+
+	$email_subject ="New Form Submission";
+
+	$email_body = "User Name: $name.\n".
+					"User Email: $visitor_email.\n".
+						"User Message:$message";
+
+	$to = "smayank.singh.1998@gmail.com";
+	
+	$headers = "From: $email_form \r\n";
+
+	$headers = "Replt-To: $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location: index.html");					
